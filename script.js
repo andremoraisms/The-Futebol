9539d121d728eeb720fef1646b3ef7eb4375737e
André Felipe <86194863+andremoraisms@users.noreply.github.com>
2024-05-22 14:31:35 -0300

    adicionando js

---
+++ script.js
@@ -0,0 +1,13 @@
+
+var navItem = document.querySelectorAll('.nav-list')
+
+function selecionarPag(){
+    navItem.forEach((pag)=>
+        item.classList.remove('ativo')
+)
+    this.classList.add('ativo')
+}
+
+navItem.forEach((pag)=>
+    pag.addEventListener('click', selecionarPag)
+)
